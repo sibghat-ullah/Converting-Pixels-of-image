e7c634727effcf0c6a454ceaeffff7ec667865cb
sibghat-ullah <48874300+sibghat-ullah@users.noreply.github.com>
2023-02-24 12:47:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
-  
-originalImage = cv2.imread('121.jpg')
+import numpy as np
+from scipy import ndimage
+
+originalImage = cv2.imread('124.jpg')
 grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
   
 (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
@@ -8,7 +10,14 @@ grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 # cv2.imshow('Original image',originalImage)
 # cv2.imshow('Gray image', grayImage)
 new = cv2.subtract(255, blackAndWhiteImage) 
-cv2.imwrite('new.png',new)
+
+# Rotating the img 
+#result = ndimage.rotate(new, -10)
+
+#cv2.imwrite('124_new.png',result)
+
 cv2.imshow('Black white image', new)
+
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
